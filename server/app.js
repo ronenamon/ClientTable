26cf93e96a56142890e64bcc13744f1c9f50813d
Ronen Amon <ronenamon88@gmail.com>
2021-01-13 22:28:06 +0200

    connection to mongoDB

--- server/app.js
+++ server/app.js
@@ -11,3 +11,33 @@ const bodyParser = require('body-parser')
 app.set('port' , (process.env.PORT || 8081))
 
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+
+
+app.use('/api' , api)
+app.use(express.static('static'))
+
+app.use(morgan('dev'))
+
+app.use(function (req,res,next){
+    const err = new Error('Not Found')
+    err.status = 404
+    res.json(err)
+})
+
+const mongoose  = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/ronenDBForVuejs')
+const db = mongoose.connection
+
+db.on('error' , console.error.bind(console,"connection error"))
+db.once('open',function () {
+    console.log('Connected to MongoDB');
+
+    app.listen(app.get('port') ,function(){
+        console.log("API server listenning on port" + app.get('port'));
+    })
+    
+})
