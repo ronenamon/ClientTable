8e8c7eaceb4ea8e9b2add3d95aa1c97f22ed9404
Ronen Amon <ronenamon88@gmail.com>
2021-01-15 11:07:54 +0200

    update api user router

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -14,7 +14,54 @@ module.exports = function(router){
                 error:err
             })
         )
+    
+    })//find by id
+
+
+    router.get('/user/email/:email' , function (reqmres) {
+     
+        User.find({'email': req.params.email}).exec()
+        .then(docs => 
+                   res.status(200).json(docs)
+        ).catch(err =>
+            res.status(500).json({
+                message:'Error user not found',
+                error:err
+            })
+        )
+    
+    })//find by email
+
+    //POST
+    router.post('/user' , function(req,res){
+        let user = new User(req.body)
+        user.save(function(err,user){
+            if(err) return console.log(err)
+            res.status(200).json(user)
+        })
     })
+
+    //update
+    router.put('/user/:id',function(req,res){
+        console.log(req.body)
+        let qry = {_id: req.params.id}
+        let doc = {
+            first:req.body.firstName,
+            last:req.body.lastName,
+            email:req.body.email,
+            password:req.body.password,
+            isActive:req.body.isActive
+        }
+        
+        console.log(doc);//test
+        User.update(qry , doc,function(err,respRaw){
+            if(err) return console.log(err);
+
+            res.status(200).json(respRaw);
+        })
+    })
+
+
 
 
 }
